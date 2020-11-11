4ada711d5fcc3b76efaa11185367516466dcab00
HarditSingh12 <68427615+HarditSingh12@users.noreply.github.com>
2020-11-11 19:50:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,21 +3,27 @@ var dogImage;
 var dog;
 var foodStock
 var food = 20;
+var feed;
+var addFood;
+var bottleImage;
 function preload()
 {
   //load images here
  dogImage = loadImage("images/dogImg.png"); 
  dogImage1 = loadImage("images/dogImg1.png");
+ bottleImage = loadImage("images/Milk (1).png");
+
 }
 
 function setup() {
-  database = firebase.database();
-  createCanvas(500, 500);
-  dog = createSprite(250,250,25,50);
-  dog.addImage("mmm",dogImage);
-  foodStock = database.ref("food");
-  foodStock.on("value",readStock)
-  
+        database = firebase.database();
+        createCanvas(500, 500);
+        dog = createSprite(350,250,25,50);
+        dog.addImage("mmm",dogImage);
+        foodStock = database.ref("food");
+        foodStock.on("value",readStock)
+
+        
 }
 
 
@@ -25,18 +31,30 @@ function draw() {
   background("green"); 
  drawSprites();
   //add styles here
+ 
+ 
+  feed = createButton("feed the dog");
+  feed.position(450,100);
+  feed.mousePressed(feedFood)
+    
+  addFood = createButton("add food");
+  addFood.position(550,100);
+  addFood.mousePressed(AddFood)
+  
 
-  if(keyDown(UP_ARROW)){
-    dog.addImage(dogImage1);
-    writeStock(food)
-  }
+  fill("black")
+text("food remaining:"+food,300,150)
 
-  if(keyDown("r")){
-    database.ref("/").update({
-      food:20
-    })
-  }
-text("food remaining:"+food,200,200)
+for(var i =30;i<270;i=i+25){
+  var bottle = createSprite(i,250,20,25);
+  bottle.addImage(bottleImage);
+ 
+}
+
+for(var x = 30;x<270;x=x+25){
+  var bottle2 = createSprite(x,280,20,25);
+  bottle2.addImage(bottleImage);
+}
 }
 
 
@@ -57,4 +75,22 @@ function writeStock(x){
   })
 }
 
+function feedFood(){
+  dog.addImage("mm",dogImage1);
+  if(food<=0){
+    food=0;
+  }
+  else{
+  food--;
+  }
+  database.ref("/").update({
+food:food
+  })
+  
+}
 
+function AddFood(){
+  database.ref("/").update({
+    food:20
+})
+}
